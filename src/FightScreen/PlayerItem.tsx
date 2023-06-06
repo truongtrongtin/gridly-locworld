1061ceeae19ec2c0c544228f290d3d6c7e06e932
Tin Truong <tin@localizedirect.com>
2023-06-06 12:39:03 +0700

    feat: only filter the card content

--- src/FightScreen/PlayerItem.tsx
+++ src/FightScreen/PlayerItem.tsx
@@ -17,7 +17,7 @@ const PlayerItem = forwardRef<HTMLDivElement, PlayerItemProps>(function (
   ref
 ) {
   return (
-    <div className={`relative ${health <= 0 ? "grayscale" : ""}`} ref={ref}>
+    <div className="relative" ref={ref}>
       {quote && (
         <motion.div className="w-[150%] absolute -top-2 -translate-y-full left-[50%] -translate-x-1/2 border-[5px] rounded-[10px] border-[#D55CFF] px-8 py-6 bg-white text-xl text-center">
           {quote}
@@ -33,20 +33,22 @@ const PlayerItem = forwardRef<HTMLDivElement, PlayerItemProps>(function (
           {critical ? "!!!" : ""}
         </motion.div>
       )}
-      <p className="absolute text-white top-[13px] left-[50%] -translate-x-1/2 text-xl whitespace-nowrap">
-        {name}
-      </p>
-      <img src={playerCard} className="w-[300px]" />
-      <img
-        src={imageUrl}
-        className="absolute w-[200px] h-[200px] top-[60px] left-[50%] -translate-x-1/2"
-      />
-      <p className="absolute text-[#2B167D] bottom-[50px] right-[60px] text-xl">
-        {health}
-      </p>
-      <p className="absolute text-[#2B167D] bottom-[25px] right-[60px] text-xl">
-        {strength}
-      </p>
+      <div className={`${health <= 0 ? "grayscale" : ""}`}>
+        <p className="absolute text-white top-[13px] left-[50%] -translate-x-1/2 text-xl whitespace-nowrap">
+          {name}
+        </p>
+        <img src={playerCard} className="w-[300px]" />
+        <img
+          src={imageUrl}
+          className="absolute w-[200px] h-[200px] top-[60px] left-[50%] -translate-x-1/2"
+        />
+        <p className="absolute text-[#2B167D] bottom-[50px] right-[60px] text-xl">
+          {health}
+        </p>
+        <p className="absolute text-[#2B167D] bottom-[25px] right-[60px] text-xl">
+          {strength}
+        </p>
+      </div>
     </div>
   );
 });
